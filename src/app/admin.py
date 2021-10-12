33a489855ec0f299cc83ea89534edbd3e34920fb
Ryohei Tsunoda <83001827+ryohei681@users.noreply.github.com>
2021-10-12 17:39:54 +0900

    remove:app

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-
-from .models import Area
-from .models import Cafe
-from .models import Utility
-
-admin.site.register(Area)
-admin.site.register(Cafe)
-admin.site.register(Utility)
