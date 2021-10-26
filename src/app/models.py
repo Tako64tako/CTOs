c1ce1302a53e44640c6301c92ba13cbe608a9494
高村隼平 <shum0324@takamurashumpeinoMacBook-Air.local>
2021-10-26 15:46:17 +0900

    DBのテーブル作成完了

--- src/app/models.py
+++ src/app/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+class Cloth(models.Model):
+    # 商品番号 : INTEGR型で、主キー
+    number = models.CharField(max_length=100,primary_key=True)
+    # 商品名 : 文字列30桁
+    name = models.CharField(max_length=30)
+    # 性別 : 文字列30桁
+    gender = models.CharField(max_length=30)
+    # 種類 : 文字列30桁
+    kind = models.CharField(max_length=30)
+    # サイズ : 文字列30桁
+    size = models.CharField(max_length=30)
+    # 色 : 文字列30桁
+    color = models.CharField(max_length=30)
+    #
+    def __str__(self):
+        return self.name;
