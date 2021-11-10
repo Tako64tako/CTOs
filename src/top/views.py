f0ec2a7c715a026096926af493222a8abad25158
松田　雄飛 <k20109kk@aitech.ac.jp>
2021-11-10 17:10:52 +0900

    ぶっ壊し前

--- src/top/views.py
+++ src/top/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from top.models import Cloth
+from django.db.models import Q
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+def list_cloth(request):
+    clother = Cloth.objects.all()
+    context = {
+        'index':"CLOTH",
+        'clothers':clother
+    }
+    return render(request,'top/templates/list.html',context)
+
