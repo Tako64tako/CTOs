34e50b29b11757fda880242df52eef53478c865a
Ryohei Tsunoda <83001827+ryohei681@users.noreply.github.com>
2021-11-30 10:35:15 +0900

    feature: slider

--- src/static/app_script/index.js
+++ src/static/app_script/index.js
@@ -1,11 +1,13 @@
-$(function() {
-    $('.hamburger').click(function() {
-        $(this).toggleClass('active');
- 
-        if ($(this).hasClass('active')) {
-            $('.globalMenuSp').addClass('active');
-        } else {
-            $('.globalMenuSp').removeClass('active');
-        }
-    });
-});
+
+$('.slider').slick({
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    speed: 500,//スライドのスピード。初期値は300。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    centerMode: true,//要素を中央ぞろえにする
+    centerPadding:'0px',
+    variableWidth: true,//幅の違う画像の高さを揃えて表示
+    dots: true,//下部ドットナビゲーションの表示
+  });
--- src/top/models.py
+++ src/top/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 import uuid
-import os
+import django_cleanup
+# from imagekit.models import ProcessedImageField
+# from imagekit.processors import ResizeToFill
+from stdimage.models import StdImageField
+
 # Create your models here.
+
+def image_directory_path(instance, filename):
+    return 'images/{}.{}'.format(str(uuid.uuid4()), filename.split('.')[-1])
 
 class Cloth(models.Model):
     # 商品番号 : INTEGR型で、主キー
     number = models.CharField(max_length=100, primary_key=True)
     # 商品名 : 文字列30桁
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=200)
     # 性別 : 文字列30桁
     gender = models.CharField(max_length=30)
     # 種類 : 文字列30桁
@@ -16,11 +23,23 @@ class Cloth(models.Model):
     size = models.CharField(max_length=30)
     # 色 : 文字列30桁
     color = models.CharField(max_length=30)
-    
-    img = models.ImageField(upload_to="image/", blank=True, null=True)
-
     #写真のURL
-    pictures = models.URLField()
+    picture_detail_url = models.URLField(max_length=1000)
+    #写真の詳細のURL
+    picture_url = models.URLField(max_length=1000)
+
+    # picture = ProcessedImageField(upload_to=image_directory_path,
+    #                                        processors=[ResizeToFill(420, 600)],
+    #                                        format='JPEG',
+    #                                        options={'quality': 60})
+
+    # 画像ファイル
+    picture = StdImageField(upload_to=image_directory_path, blank=True, variations={
+        'large': (420, 600),
+        'medium': (200, 300),
+    })
+
+
+
     def __str__(self):
         return self.name
-
